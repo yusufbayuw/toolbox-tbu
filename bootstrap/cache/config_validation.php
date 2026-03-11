6c58d7d986022f3d51d81e44a3c80eba4a1d1d04
Yusuf Bayu Wicaksono <yusufbayu31@gmail.com>
2026-03-11 23:57:10 +0700

    Add integrity manifest

---
+++ bootstrap/cache/config_validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+  'generated_at' => '2026-03-11T16:41:40+00:00',
+  'files' => 
+  array (
+    'app/Http/Controllers/LicenseController.php' => '686f2c796b2e0a301f53bf8e4af0d08d0ba1977d73f7131c0efab707382c259e',
+    'app/Http/Middleware/VerifyConfigIntegrity.php' => '2b79a8dcfc850a9eb32a3a1f4a25158bb4a01bfa9e0133591b6a5dc578803caa',
+    'app/Providers/AppServiceProvider.php' => '6f628eb1afe381279104e9a362baf0a93348a7ec124b5d700ecbe071d0a32ce9',
+    'app/Providers/Filament/AdminPanelProvider.php' => 'bff71be7d3da8dfe9d3b13a75f7cb1ac52566d4167f3c20bb5657fe5075705fe',
+    'app/Services/License/LicenseService.php' => 'c4579c71d5a96282c79dd1b519a8978a11155fb5117491ab0d9a86e36c415bdb',
+    'app/Support/SystemBoot.php' => '52d57547912cb9cfe7e1217be072d5d977d5e46b3f26a15c1b9bc25bf04516e2',
+    'resources/views/certificate/template.blade.php' => '0c2743e1442ce3206398f4dc85f9973aadbc530828a97df2778f55507bce9a48',
+    'resources/views/certificate/validation.blade.php' => '25bc3e3744525adecdad194c6f02a11850e89eb65a175535c7a9250ae1e3fc63',
+    'resources/views/custom/footer.blade.php' => 'aeca800dcb084de78230ea19be2628e9bc7219fc11a68b7bcd49f12d045e5ce6',
+    'resources/views/tree.blade.php' => 'b37d3348bb3ea6155904673a10a458234da1f45147bfdfe0d6906cf9d8a4bf91',
+  ),
+);
